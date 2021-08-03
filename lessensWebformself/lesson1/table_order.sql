a222e04be19baa3947a69c53fd13382880216059
chenagoold <chenagoold@gmail.com>
2021-08-03 23:02:37 +0300

    lessons1

---
+++ lessensWebformself/lesson1/table_order.sql
@@ -0,0 +1,16 @@
+//создание БД 
+CREATE DATABASE `sql` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+//просмотр БД
+show databases;
+
+//создание таблиц в БД
+CREATE TABLE orders (
+	onum INT(11) NOT NULL,
+	amt DECIMAL(6,2) NOT NULL,
+	odate DATE NOT NULL,
+	cnum INT(11) NOT NULL,
+	snum INT(11) NOT NULL,
+	PRIMARY KEY(onum)
+) ENGINE=MyISAM;
+
